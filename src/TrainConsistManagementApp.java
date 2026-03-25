52019df3f41a8d34ed2eae512f6381753ed01105
Nirik-11 <ninirik11@gmail.com>
2026-03-25 08:44:08 +0530

    UC1: WELCOME MESSAGER

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,2 +1,7 @@
 public class TrainConsistManagementApp {
+    public static void main(String[] args) {
+        System.out.println("Train Initialized Sucessfully");
+        System.out.println("Initial Bogie Count:0");
+        System.out.println("Current Train Consist:[]");
+    }
 }
