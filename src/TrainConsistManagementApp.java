0d6168d712dafae84aed28f2c98dadbcbc9d5974
Nirik-11 <ninirik11@gmail.com>
2026-03-25 09:38:13 +0530

    UC5:Hardcoded palindrome

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,37 +1,23 @@
-import java.util.LinkedList;
+import java.util.LinkedHashSet;
 
 public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
 
 
-        LinkedList<String> trainConsist = new LinkedList<>();
+        LinkedHashSet<String> trainFormation = new LinkedHashSet<>();
+
+        // Add bogies
+        trainFormation.add("Engine");
+        trainFormation.add("Sleeper");
+        trainFormation.add("Cargo");
+        trainFormation.add("Guard");
 
 
-        trainConsist.add("Engine");
-        trainConsist.add("Sleeper");
-        trainConsist.add("AC");
-        trainConsist.add("Cargo");
-        trainConsist.add("Guard");
-
-        System.out.println("Initial Train Consist:");
-        System.out.println(trainConsist);
+        trainFormation.add("Sleeper"); // duplicate, will be ignored
 
 
-        trainConsist.add(2, "Pantry Car");
-
-        System.out.println("\nAfter adding Pantry Car at position 2:");
-        System.out.println(trainConsist);
-
-
-        trainConsist.removeFirst();
-        trainConsist.removeLast();
-
-        System.out.println("\nAfter removing first and last bogie:");
-        System.out.println(trainConsist);
-
-
-        System.out.println("\nFinal Train Consist:");
-        System.out.println(trainConsist);
+        System.out.println("Final Train Formation (Ordered & Unique):");
+        System.out.println(trainFormation);
     }
 }
