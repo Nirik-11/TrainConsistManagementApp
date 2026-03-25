1a0ff7ad990b96046fce85bac9f032029d0f9a8b
Nirik-11 <ninirik11@gmail.com>
2026-03-25 09:01:47 +0530

    UC2:Hardcoded palindrome

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,7 +1,36 @@
-public class TrainConsistManagementApp {
+import java.util.ArrayList;
+
+public class TrainConsistManagement {
+
     public static void main(String[] args) {
-        System.out.println("Train Initialized Sucessfully");
-        System.out.println("Initial Bogie Count:0");
-        System.out.println("Current Train Consist:[]");
+
+
+        ArrayList<String> passengerBogies = new ArrayList<>();
+
+
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+
+
+        System.out.println("Passenger Bogies after addition:");
+        System.out.println(passengerBogies);
+
+
+        passengerBogies.remove("AC Chair");
+
+
+        System.out.println("\nPassenger Bogies after removal of AC Chair:");
+        System.out.println(passengerBogies);
+
+
+        if (passengerBogies.contains("Sleeper")) {
+            System.out.println("\nSleeper bogie exists in the train.");
+        } else {
+            System.out.println("\nSleeper bogie does not exist in the train.");
+        }
+
+        System.out.println("\nFinal Passenger Bogies List:");
+        System.out.println(passengerBogies);
     }
 }
